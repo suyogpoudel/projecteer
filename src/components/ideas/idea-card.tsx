1ac273ea30eebd26fd1d2af6abededd9b99b2075
Suyog Poudel <suyogpoudel583@gmail.com>
2026-03-01 18:40:12 +0545

    make idea cards separate components

---
+++ src/components/ideas/idea-card.tsx
@@ -0,0 +1,178 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { db } from "@/db/drizzle";
+import { user } from "@/db/schema";
+import { mockProjects } from "@/lib/mock-projects";
+import { eq } from "drizzle-orm";
+import { ArrowBigUpDash, Star } from "lucide-react";
+import Link from "next/link";
+
+const IdeaCard = async () => {
+  const projectsWithAuthor = await Promise.all(
+    mockProjects.map(async (project) => {
+      const [author] = await db
+        .select()
+        .from(user)
+        .where(eq(user.id, project.userId));
+
+      const username = author?.username || "unknown";
+
+      return {
+        ...project,
+        username,
+      };
+    }),
+  );
+
+  return (
+    <div className="flex flex-col gap-10 md:w-[50%]">
+      {projectsWithAuthor.map((project) => {
+        const ratings = [
+          {
+            rating: project.difficultyRating,
+            label: "Difficulty",
+            description: project.difficultyDescription,
+          },
+          {
+            rating: project.usabilityRating,
+            label: "Usability",
+            description: project.usabilityDescription,
+          },
+          {
+            rating: project.hireabilityRating,
+            label: "Hireability",
+            description: project.hireabilityDescription,
+          },
+          {
+            rating: project.absurdityRating,
+            label: "Absurdity",
+            description: project.absurdityDescription,
+          },
+        ];
+
+        return (
+          <Card
+            key={project.id}
+            className="w-full"
+          >
+            <CardHeader>
+              <div className="w-full items-center flex justify-between gap-3">
+                <CardTitle className="text-lg">{project.title}</CardTitle>
+                <CardDescription>
+                  <Link
+                    href={`/u/${project.username}`}
+                    className="hover:text-primary transition-all duration-200 text-lg"
+                  >
+                    @{project.username}
+                  </Link>
+                </CardDescription>
+              </div>
+
+              <p className="italic text-primary mb-4">{project.hook}</p>
+              <Separator />
+            </CardHeader>
+
+            <CardContent className="flex flex-col gap-4">
+              <p className="text-lg text-primary">{project.description}</p>
+              <Separator />
+
+              <div className="w-full p-2 flex max-md:flex-col gap-4">
+                <div className="w-full p-2 flex flex-col gap-4">
+                  <p className="font-bold text-primary text-lg">
+                    Learning Value
+                  </p>
+                  <ul className="list-disc list-inside flex flex-col gap-2">
+                    {project.learningValue.map((value) => (
+                      <li key={value}>{value}</li>
+                    ))}
+                  </ul>
+                </div>
+
+                <div className="flex flex-col max-md:w-full w-[85%]  gap-4 p-4 md:border-l max-md:border-t border-border">
+                  <p className="font-bold text-primary text-lg">Tech Stack</p>
+                  <div className="flex flex-wrap gap-3">
+                    {project.techStack.map((tech) => (
+                      <Badge
+                        key={tech}
+                        variant="secondary"
+                      >
+                        {tech}
+                      </Badge>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
+              <Separator />
+
+              <div className="grid grid-cols-2 max-md:grid-cols-1 gap-4">
+                {ratings.map((rating) => (
+                  <Card key={rating.label}>
+                    <CardTitle className="text-center">
+                      {rating.label}
+                    </CardTitle>
+                    <CardContent className="flex flex-col items-center justify-center">
+                      <p className="text-sm mb-2">
+                        <span className="text-primary text-3xl font-extrabold">
+                          {rating.rating}
+                        </span>
+                        /5
+                      </p>
+
+                      <p className="text-sm text-muted-foreground text-center">
+                        {rating.description}
+                      </p>
+                    </CardContent>
+                  </Card>
+                ))}
+              </div>
+
+              <Separator />
+            </CardContent>
+
+            <CardFooter className="flex justify-between">
+              <div className="flex items-center gap-2">
+                <Button
+                  variant={"outline"}
+                  size={"icon"}
+                >
+                  <ArrowBigUpDash />
+                </Button>
+                <p>
+                  <span className="text-primary text-lg font-bold">
+                    {project.upvotes}
+                  </span>{" "}
+                  Upvotes
+                </p>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <p>
+                  <span className="text-primary text-lg font-bold">0</span>{" "}
+                  Saved
+                </p>
+                <Button
+                  variant={"outline"}
+                  size={"icon"}
+                >
+                  <Star />
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default IdeaCard;
