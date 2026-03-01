5888b0783810cbe755395819c16eb8f3f5fe8a39
Suyog Poudel <suyogpoudel583@gmail.com>
2026-03-01 18:54:41 +0545

    improve tech stack keys for mapping

--- src/components/ideas/tech-stack.tsx
+++ src/components/ideas/tech-stack.tsx
@@ -5,9 +5,9 @@ const TechStack = ({ techStack }: { techStack: string[] }) => {
     <div className="flex flex-col max-md:w-full w-[85%]  gap-4 p-4 md:border-l max-md:border-t border-border">
       <p className="font-bold text-primary text-lg">Tech Stack</p>
       <div className="flex flex-wrap gap-3">
-        {techStack.map((tech) => (
+        {techStack.map((tech, index) => (
           <Badge
-            key={tech}
+            key={`${tech}-${index}`}
             variant="secondary"
           >
             {tech}
